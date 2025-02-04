f01a18ab6f23762518f89e7bed3431c78b079734
nazim1971 <md.nazimuddinaj@gmail.com>
2025-02-04 14:08:42 +0600

    Add progatamacillay navigate

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,22 @@
-
+"use client"
+import { useRouter } from "next/navigation";
 
 const DashboardPage = () => {
-    return (
-        <div>
-          This is Dashboard page  
-        </div>
-    );
+  const router = useRouter();
+  const handleNavigate = () => {
+    router.push('/');
+  }
+  return (
+    <div className="text-center ">
+      <h1 className="text-3xl mb-4">This is Dashboard page </h1>
+
+      <div>
+        <button onClick={handleNavigate} className="bg-blue-500 p-3 rounded-lg">
+          Home
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,26 +1,28 @@
+import Link from 'next/link';
+
 const Navbar = () => {
     return (
         <nav className="bg-gray-800 p-4">
             <ul className="flex space-x-4 justify-center">
                 <li>
-                    <a href="/about" className="text-white hover:text-gray-400">
+                    <Link  href="/about" className="text-white hover:text-gray-400">
                         About
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a href="/counter" className="text-white hover:text-gray-400">
+                    <Link href="/counter" className="text-white hover:text-gray-400">
                         Counter
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a href="/dashboard" className="text-white hover:text-gray-400">
+                    <Link href="/dashboard" className="text-white hover:text-gray-400">
                         Dashboard
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a href="/products" className="text-white hover:text-gray-400">
+                    <Link href="/products" className="text-white hover:text-gray-400">
                         Product
-                    </a>
+                    </Link>
                 </li>
             </ul>
         </nav>
