245a201830fc357f1be62287bf5ecbc036280876
nazim1971 <md.nazimuddinaj@gmail.com>
2025-02-05 17:17:29 +0600

    handle error page

---
+++ src/app/(commonLayout)/products/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const ErrorPage = () => {
+
+
+    return (
+        <div>
+            <h1 className="text-4xl text-center mt-10 ">Something went wrong</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/app/(commonLayout)/products/loading.tsx
@@ -0,0 +1,11 @@
+
+
+const Loading = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-center ">Loading</h1>
+        </div>
+    );
+};
+
+export default Loading;
