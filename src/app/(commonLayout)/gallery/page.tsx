1a5bf138ced2b2e19378dcb92e2e4fa63d5ec4ea
nazim1971 <md.nazimuddinaj@gmail.com>
2025-02-05 13:48:10 +0600

    organized Image tag

--- src/app/(commonLayout)/gallery/page.tsx
+++ src/app/(commonLayout)/gallery/page.tsx
@@ -1,19 +1,15 @@
 import Image from "next/image";
-
+import localImg from '../../../assets/localImg.avif'
 
 const GalleryPage = () => {
     return (
         <div className="text-center mx-auto">
-           <h1>Regular image</h1> 
-           <div>
-           <img src="https://res.cloudinary.com/dfvgxf4dc/image/upload/v1738576601/man-choosed-motorcycles-moto-shop-guy-black-jacket-manager-with-client_1157-43592_tcjzsm.avif" alt="" />
-           </div>
 
            <h1>Next js image </h1>
            <Image
            width={500}
            height={500}
-           src={'https://res.cloudinary.com/dfvgxf4dc/image/upload/v1738576601/man-choosed-motorcycles-moto-shop-guy-black-jacket-manager-with-client_1157-43592_tcjzsm.avif'} alt="A guy buy motor cycle" />
+           src={localImg} alt="A guy buy motor cycle" />
         </div>
     );
 };
